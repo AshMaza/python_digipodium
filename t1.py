a3bbc0dc5db6a659f6b9e8b7717b8aa92389c2a0
AshMaza <asham.zaidi@gmail.com>
2023-05-29 17:10:44 +0530

    first commit

---
+++ t1.py
@@ -0,0 +1,20 @@
+from turtle import * #import all functions from turtle module
+speed('slowest')
+
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+lt(60)
+fd(100)
+
+mainloop() #KEEP THE WINDOW OPEN UNTIL IT IS CLOSED
+
